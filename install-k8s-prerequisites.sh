2dfd1988ee6453d23477ea122db48b72df179fb5
Vijay Pandian <ananth.vijay@gmail.com>
2023-06-05 15:39:25 -0400

    Add --update flag during install

--- install-k8s-prerequisites.sh
+++ install-k8s-prerequisites.sh
@@ -25,4 +25,4 @@ eksctl version
 echo "Install awscli..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
